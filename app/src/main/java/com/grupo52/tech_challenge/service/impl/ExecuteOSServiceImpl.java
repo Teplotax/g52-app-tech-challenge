d6bcc7fc2f46e92fab17a6415717162209658148
Teplotax <eq.tfreis@gmail.com>
2026-06-23 18:48:40 -0300

    Created ExecuteOSService

---
+++ app/src/main/java/com/grupo52/tech_challenge/service/impl/ExecuteOSServiceImpl.java
@@ -0,0 +1,39 @@
+package com.grupo52.tech_challenge.service.impl;
+
+import com.grupo52.tech_challenge.domain.Enums.StatusOS;
+import com.grupo52.tech_challenge.domain.OrdemDeServico;
+import com.grupo52.tech_challenge.exception.GatewayException;
+import com.grupo52.tech_challenge.exception.ServiceException;
+import com.grupo52.tech_challenge.exception.ValidationException;
+import com.grupo52.tech_challenge.gateway.FindOSGateway;
+import com.grupo52.tech_challenge.gateway.UpdateOSGateway;
+import com.grupo52.tech_challenge.service.ExecuteOSService;
+import com.grupo52.tech_challenge.service.UpdateOSStatusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExecuteOSServiceImpl implements ExecuteOSService {
+
+    @Autowired
+    private FindOSGateway findOSGateway;
+
+    @Autowired
+    private UpdateOSGateway updateOSGateway;
+
+    @Autowired
+    private UpdateOSStatusService updateOSStatusService;
+
+    @Override
+    public OrdemDeServico execute(Long osId) throws GatewayException, ValidationException, ServiceException {
+        try {
+            OrdemDeServico os = findOSGateway.execute(osId);
+            updateOSStatusService.execute(os, StatusOS.EM_EXECUCAO);
+            return updateOSGateway.execute(os);
+        } catch (ValidationException | GatewayException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ServiceException("Falha inesperada ao atualizar OS para EM_EXECUCAO", e);
+        }
+    }
+}
