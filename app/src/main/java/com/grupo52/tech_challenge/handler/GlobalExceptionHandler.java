cf729888de8347f4b50213d5dce0c517c0414695
Teplotax <eq.tfreis@gmail.com>
2026-06-26 22:07:47 -0300

    Updated exception handler

--- app/src/main/java/com/grupo52/tech_challenge/handler/GlobalExceptionHandler.java
+++ app/src/main/java/com/grupo52/tech_challenge/handler/GlobalExceptionHandler.java
@@ -13,14 +13,14 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.*;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -104,6 +104,13 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.valueOf(e.getStatus())).body(
                 new DefaultErrorMessage(e.getMessage(), HttpStatus.valueOf(e.getStatus()).toString()));
+    }
+
+    @ExceptionHandler({NoResourceFoundException.class})
+    public ResponseEntity<DefaultErrorMessage> handleNoResourceFoundException(final Exception e) {
+
+        return ResponseEntity.status(NOT_FOUND).body(
+                new DefaultErrorMessage("Recurso não encontrado", NOT_FOUND.toString()));
     }
 
     @ExceptionHandler({Exception.class})
